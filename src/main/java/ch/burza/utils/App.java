689cf94a40cef11549173d9fa2a1828648e395e1
Norbert Burza <norbert.burza@gmail.com>
2017-08-22 17:26:12 -0400

    Socket number reduced to 2, sending a simple message

--- src/main/java/ch/burza/utils/App.java
+++ src/main/java/ch/burza/utils/App.java
@@ -4,47 +4,52 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collection;
 import java.util.ArrayList;
+import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
+import java.io.PrintWriter;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
+public class App {
+    public static void main(String[] args) throws Exception {
+        // A list of sockets
         Collection<SSLServerSocket> serverSockets = new ArrayList<>();
 
         SSLServerSocketFactory sslServerSocketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
 
-        for (int port = 10000; port < 10010; port++) {
+        // Creating SSL server sockets
+        for (int port = 10000; port < 10002; port++) {
             serverSockets.add((SSLServerSocket) sslServerSocketFactory.createServerSocket(port));
         }
 
-
+        // Start a thread where client sockets will be created
         Runnable clientThread = new ClientThread();
         new Thread(clientThread).start();
 
+        // Accepts connections from clients and sending them a message
         for (SSLServerSocket serverSocket : serverSockets) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Accepted");
-            socket.getOutputStream().write(7);
-            socket.getOutputStream().flush();
+            SSLSocket socket = (SSLSocket) serverSocket.accept();
+            System.out.println("Connection for port=" + socket.getLocalPort() + " accepted.");
+            socket.setUseClientMode(false);
+            System.out.println("Server starts handshake...");
+            socket.startHandshake();
+            // Send a sample message to the client
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println("A simple message from localPort=" + socket.getLocalPort());
+            writer.flush();
         }
 
+        // Wait 3 seconds
         try {
-            
-
-Thread.sleep(3 * 1000);
+            Thread.sleep(3 * 1000);
         } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-        } finally {
-
+            System.err.println(e.getMessage());
         }
 
+        // Close all sockets
         for (ServerSocket serverSocket : serverSockets) {
             serverSocket.close();
         }
--- src/main/java/ch/burza/utils/ClientThread.java
+++ src/main/java/ch/burza/utils/ClientThread.java
@@ -3,7 +3,9 @@ package ch.burza.utils;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
+import java.io.BufferedReader;
 import java.io.PrintWriter;
+import java.io.InputStreamReader;
 import java.net.Inet6Address;
 import java.net.Socket;
 
@@ -12,19 +14,19 @@ public class ClientThread implements Runnable {
     public void run() {
         SSLSocketFactory sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
 
-        for (int port = 10000; port < 10010; port++) {
-            System.out.println("creating a socket for " + port);
+        for (int port = 10000; port < 10002; port++) {
+            System.out.println("Connecting to localhost:" + port);
             try {
-                Thread.sleep(1000);
+                //Create a client SSL socket
                 SSLSocket socket = (SSLSocket) sslSocketFactory.createSocket(Inet6Address.getLocalHost(), port);
-                socket.startHandshake();;
-                System.out.println("isConnected " + socket.isConnected());
-                PrintWriter pw = new PrintWriter(socket.getOutputStream());
-                
-                
+                System.out.println("Client's starting handshake...");
+                socket.startHandshake();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                System.out.println("Received: " + bufferedReader.readLine());
                 Thread.sleep(1000);
             } catch (Exception e) {
-                e.printStackTrace();
+                System.err.println(e.getMessage());
             }
         }
     }
